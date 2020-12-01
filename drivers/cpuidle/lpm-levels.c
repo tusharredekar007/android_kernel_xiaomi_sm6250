c28eac4a6b629373394afaa382ef806ad092a0a6
Sultan Alsawaf <sultan@kerneltoast.com>
2020-11-30 19:13:14 -0800

    Revert "drivers: cpuidle: lpm-levels: check for valid LPM stats"
    This reverts commit ebced8158b49486bf9666620cfe8a42b6d395047.

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1229,8 +1229,7 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 	if (cluster_configure(cluster, i, from_idle, predicted))
 		goto failed;
 
-	if (!IS_ERR_OR_NULL(cluster->stats))
-		cluster->stats->sleep_time = start_time;
+	cluster->stats->sleep_time = start_time;
 	cluster_prepare(cluster->parent, &cluster->num_children_in_sync, i,
 			from_idle, start_time);
 
@@ -1238,8 +1237,7 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 	return;
 failed:
 	spin_unlock(&cluster->sync_lock);
-	if (!IS_ERR_OR_NULL(cluster->stats))
-		cluster->stats->sleep_time = 0;
+	cluster->stats->sleep_time = 0;
 }
 
 static void cluster_unprepare(struct lpm_cluster *cluster,
@@ -1278,7 +1276,7 @@ static void cluster_unprepare(struct lpm_cluster *cluster,
 	if (!first_cpu || cluster->last_level == cluster->default_level)
 		goto unlock_return;
 
-	if (!IS_ERR_OR_NULL(cluster->stats) && cluster->stats->sleep_time)
+	if (cluster->stats->sleep_time)
 		cluster->stats->sleep_time = end_time -
 			cluster->stats->sleep_time;
 	lpm_stats_cluster_exit(cluster->stats, cluster->last_level, success);
@@ -1753,9 +1751,6 @@ static void register_cluster_lpm_stats(struct lpm_cluster *cl,
 
 	cl->stats = lpm_stats_config_level(cl->cluster_name, level_name,
 			cl->nlevels, parent ? parent->stats : NULL, NULL);
-	if (IS_ERR_OR_NULL(cl->stats))
-		pr_info("Cluster (%s) stats not registered\n",
-			cl->cluster_name);
 
 	kfree(level_name);
 
